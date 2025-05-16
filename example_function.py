7c02a0d635f449aa9a402e54cc00e13ee21f4685
Marc Pickett <mpickett@merlyn.org>
2025-05-16 10:07:24 -0400

    Push it.

--- example_function.py
+++ example_function.py
@@ -1,6 +1,6 @@
 # This is a placeholder for YOUR function.  The API should be clear from below.
 
-def hello_world_process_question(haystack_sessions: list[list[dict]], question: str, question_date:str, haystack_dates: list[str]) -> str:
+def hello_world_process_question(haystack_sessions: list[list[dict]], question: str, question_date:str, haystack_dates: list[str]) -> strw:
     # haystack_sessions is a list of sessions, each session is a list of turns, each turn is a dict with role and content
     # question_date : '2023/08/20 (Sun) 23:59'
     # return hypothesis: str
---
+++ example_rest_api.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import json, requests
+
+def json_rest_process_question(haystack_sessions: list[list[dict]], question: str, question_date:str, haystack_dates: list[str]):
+    big_json = {
+        'haystack_sessions': haystack_sessions,
+        'question': question,
+        'question_date': question_date,
+        'haystack_dates': haystack_dates
+    }
+    return json.dumps(big_json)
+
+def call_rest_endpoint(haystack_sessions: list[list[dict]], question: str, question_date:str, haystack_dates: list[str]):
+    # Call REST endpoint
+    url = "http://localhost:5000/process_question"
+    headers = {'Content-Type': 'application/json'}
+    big_json = json_rest_process_question(haystack_sessions, question, question_date, haystack_dates)
+    response = requests.post(url, headers=headers, data=big_json)
+    return response.json()['answer']
+
+# Example of a REST endpoint that would receive the JSON data
+def rest_endpoint(big_json_str:str):
+    # This will be on the receiving end of the REST endpoint
+    big_json = json.loads(big_json_str)
+    haystack_sessions = big_json['haystack_sessions']
+    question = big_json['question']
+    question_date = big_json['question_date']
+    haystack_dates = big_json['haystack_dates']
+    # Do something with the data
+    answer = "I don't know the answer to that!"
+    return answer
+
+####
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+@app.route('/process_question', methods=['POST'])
+def process_question():
+    # Get the JSON data from the request
+    data = request.get_json()
+    # Process the data
+    answer = rest_endpoint(json.dumps(data))
+    # Return a JSON response
+    return jsonify({'answer': answer})
+
+def test():
+    haystack_sessions = [[{"role": "user", "content": "Hello"}]]
+    question = "What is the capital of France?"
+    question_date = "2023/08/20 (Sun) 23:59"
+    haystack_dates = ["2023/08/20 (Sun)"]
+    response = call_rest_endpoint(haystack_sessions, question, question_date, haystack_dates)
+    print(response)
+
+def main(): app.run(debug=True, port=5000)
+
+if __name__ == '__main__': main()
