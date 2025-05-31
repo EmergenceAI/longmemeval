f22b05572afe09af012aad5721549c56d542405a
Marc Pickett <mpickett@merlyn.org>
2025-05-31 07:54:59 -0400

    Push it.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from algos.example_prakhar_bot import process_question as process_func
 
 ### Edit ^^^ to point to your function.
 
-import json, os, inspect, hashlib
+import json, os
 from utils import Evaluator, predict_with_early_stopping, evaluate_qa, DumbLogger
 
 DATA_DIR = './data/'
@@ -29,13 +29,6 @@ def run_expt():
     b_successes = 0
     b_nobs = 0
     tolerance = 0.05 # Not used if b_nobs > 0
-    #
-    # Print stuff about the process function using inspect.
-    print(f'process_func name: {process_func.__name__}')
-    print(f'process_func docstring: {inspect.getdoc(process_func)}')
-    print(f'process_func source hash: {hashlib.sha256(inspect.getsource(process_func).encode()).hexdigest()}')
-    print(f'process_func module: {process_func.__module__}')
-    print(f'process_func file: {inspect.getfile(process_func)}')
     #
     hypotheses, num_success, nobs = predict_with_early_stopping(haystacks, process_func, evaluator, confidence, b_successes, b_nobs, tolerance, verbose=False)
     print(f'Evaluated {nobs} hypotheses with {num_success} successes.  Accuracy: {num_success / nobs:.4f}')
